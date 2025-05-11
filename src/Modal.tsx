2340d306e4b68205016945c9dd21e99f0fd3f985
Daniel Whiffing <danielwhiffing@gmail.com>
2025-05-11 13:06:54 -0400

    Prevent closing modal via overlay

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -12,7 +12,6 @@ export const Modal = ({
       className={`fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 opacity-0 transition-opacity duration-700 ${
         !isOpen ? 'pointer-events-none' : 'opacity-100 pointer-events-auto'
       }`}
-      onClick={onClose}
     >
       <div
         className={`bg-[#444] rounded-xl p-6 max-w-lg w-full relative shadow-lg flex h-full max-h-[600px] opacity-0 transition-opacity duration-700 ${
